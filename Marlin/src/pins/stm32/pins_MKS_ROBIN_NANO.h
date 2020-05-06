a6c3fa771a577fe5c36030ecedc34008cbe7fea7
Emperor <leetspeak@me.com>
2020-05-06 23:09:39 +0200

    Update pins_MKS_ROBIN_NANO.h

--- Marlin/src/pins/stm32/pins_MKS_ROBIN_NANO.h
+++ Marlin/src/pins/stm32/pins_MKS_ROBIN_NANO.h
@@ -147,7 +147,7 @@
 // Misc. Functions
 //
 #define POWER_LOSS_PIN     PA2   // PW_DET
-#define PS_ON_PIN          PA3   // PW_OFF
+//#define PS_ON_PIN          PA3   // PW_OFF
 
 #define LED_PIN            PB2
 
