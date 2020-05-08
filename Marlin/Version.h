10eff653f44a3537a6f83f50155b340c1e448238
Emperor <leetspeak@me.com>
2020-05-08 13:19:57 +0200

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -21,14 +21,11 @@
  */
 #pragma once
 
-////////////////////////////
-// VENDOR VERSION EXAMPLE //
-////////////////////////////
-
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "2.0.5"
+#define SHORT_BUILD_VERSION "2.0.5.X"
+#define WEBSITE_URL "http://marlinfw.org"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,12 +38,12 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-//#define STRING_DISTRIBUTION_DATE "2020-01-31"
+#define STRING_DISTRIBUTION_DATE "02.05.2020"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-//#define MACHINE_NAME "3D Printer"
+#define MACHINE_NAME "Sapphire Pro"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -54,18 +51,18 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/le3tspeak/Marlin-2.0.X-Sapphire-PRO"
 
 /**
  * Default generic printer UUID.
  */
-//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
 
 /**
  * The WEBSITE_URL is the location where users can get more information such as
  * documentation about a specific Marlin release.
  */
-//#define WEBSITE_URL "http://marlinfw.org"
+#define WEBSITE_URL "http://marlinfw.org"
 
 /**
  * Set the vendor info the serial USB interface, if changable
