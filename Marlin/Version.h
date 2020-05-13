7b1b133d4c28fb5e76af5a3e9b2555a19798ec75
Emperor <leetspeak@me.com>
2020-05-13 21:08:20 +0200

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -38,7 +38,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "02.05.2020"
+#define STRING_DISTRIBUTION_DATE "13.05.2020"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
