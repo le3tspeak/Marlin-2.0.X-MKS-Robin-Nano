844b963272a58d29b30e6869fd9993c56858e363
Emperor <leetspeak@me.com>
2020-05-18 20:36:55 +0200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1513,7 +1513,7 @@
   // K = 0 - but will be activated via gcode filament-type-specific
   // extruder seems to rattle quite hard, need to check acc/jerk settings
 
-  //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
+  #define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
