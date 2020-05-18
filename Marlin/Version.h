17885060e7887bd7742af57effcf8c0c9cdc057a
Emperor <leetspeak@me.com>
2020-05-18 20:53:52 +0200

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -25,20 +25,19 @@
  * Marlin release version identifier
  */
 #define SHORT_BUILD_VERSION "2.0.5.X"
-#define WEBSITE_URL "http://marlinfw.org"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-//#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github)"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "https://github.com/le3tspeak/Marlin-2.0.X-Sapphire-PRO/tree/2.0.5.3-EEPROM"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "13.05.2020"
+#define STRING_DISTRIBUTION_DATE "18.05.2020"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
@@ -51,12 +50,12 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-#define SOURCE_CODE_URL "https://github.com/le3tspeak/Marlin-2.0.X-Sapphire-PRO"
+#define SOURCE_CODE_URL "https://github.com/le3tspeak/Marlin-2.0.X-Sapphire-PRO/tree/2.0.5.3-EEPROM"
 
 /**
  * Default generic printer UUID.
  */
-#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
+//#define DEFAULT_MACHINE_UUID "cede2a2f-41a2-4748-9b12-c55c62f367ff"
 
 /**
  * The WEBSITE_URL is the location where users can get more information such as
