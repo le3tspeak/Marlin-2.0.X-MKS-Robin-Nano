e75dee0da39b14b6cd6f8ff8fce0db75f166bb7c
Emperor <leetspeak@me.com>
2020-05-26 18:31:04 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1143,24 +1143,23 @@
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
 #if ENABLED (Sapphire_Pro)
-#define INVERT_E0_DIR false
-#define INVERT_E1_DIR false
-#define INVERT_E2_DIR false
-#define INVERT_E3_DIR false
-#define INVERT_E4_DIR false
-#define INVERT_E5_DIR false
-#define INVERT_E6_DIR false
-#define INVERT_E7_DIR false
+ #define INVERT_E1_DIR false
+ #define INVERT_E2_DIR false
+ #define INVERT_E3_DIR false
+ #define INVERT_E4_DIR false
+ #define INVERT_E5_DIR false
+ #define INVERT_E6_DIR false
+ #define INVERT_E7_DIR false
 #endif
 #if ENABLED (Sapphire_Plus)
-#define INVERT_E0_DIR true
-#define INVERT_E1_DIR false
-#define INVERT_E2_DIR false
-#define INVERT_E3_DIR false
-#define INVERT_E4_DIR false
-#define INVERT_E5_DIR false
-#define INVERT_E6_DIR false
-#define INVERT_E7_DIR false
+ #define INVERT_E0_DIR true
+ #define INVERT_E1_DIR false
+ #define INVERT_E2_DIR false
+ #define INVERT_E3_DIR false
+ #define INVERT_E4_DIR false
+ #define INVERT_E5_DIR false
+ #define INVERT_E6_DIR false
+ #define INVERT_E7_DIR false
 #endif
 
 
