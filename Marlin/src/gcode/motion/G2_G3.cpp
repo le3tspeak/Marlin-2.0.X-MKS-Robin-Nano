bdcca68a03b954b7ce789323d02992b62ecf6da3
Emperor <leetspeak@me.com>
2020-06-09 23:06:13 +0200

    improve G2/3 movement buffer

--- Marlin/src/gcode/motion/G2_G3.cpp
+++ Marlin/src/gcode/motion/G2_G3.cpp
@@ -218,7 +218,7 @@ void plan_arc(
       planner.apply_leveling(raw);
     #endif
 
-    if (!planner.buffer_line(raw, scaled_fr_mm_s, active_extruder, seg_length
+    if (!planner.buffer_line(raw, scaled_fr_mm_s, active_extruder, 0
       #if ENABLED(SCARA_FEEDRATE_SCALING)
         , inv_duration
       #endif
