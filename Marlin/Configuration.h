1e859d163589300ff12207cf677044eafda36e0b
Emperor <leetspeak@me.com>
2020-06-18 15:55:53 +0200

    ADD PIO MKS Robin Nano -DSS_TIMER=4

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -911,7 +911,6 @@
     //No Preset
     #define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
     #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-    #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
     #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
     #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
     #define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
