41f88f707eebcb65566a8c9d07b478c435780168
Emperor <leetspeak@me.com>
2020-06-18 12:26:12 +0200

    Add JD_HANDLE_SMALL_SEGMENTS option

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1176,6 +1176,8 @@
  */
 #if DISABLED(CLASSIC_JERK)
   #define JUNCTION_DEVIATION_MM 0.019 // (mm) Distance from real junction edge
+  #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
+                                      // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
 
 /**
